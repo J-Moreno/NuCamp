814805953ecc2e41159d01eb42394ed66433500e
J. Moreno <jmoreno@regis.edu>
2019-12-15 23:59:00 -0800

    Controlled Forms and Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from '../components/CampsiteInfoComponent';
 import { COMMENTS } from '../shared/comments';
@@ -8,6 +7,8 @@ import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Directory from './DirectoryComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -32,12 +33,27 @@ class Main extends Component {
         partner={this.state.partners.filter(partner => partner.featured)[0]}
       />);
     }
+
+    const CampsiteWithId = ({match}) => {
+      return (
+        <CampsiteInfo 
+          campsite={this.state.campsites.filter(campsite => campsite.id === 
+          +match.params.campsiteId)[0]}
+          comments={this.state.comments.filter(comment => comment.campsiteId === 
+          +match.params.campsiteId)} 
+
+        />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
+          <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Route path='/directory/:campsiteId' component={CampsiteWithId} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
